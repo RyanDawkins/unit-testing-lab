b65edaa3c9f27f543c3126127e616fde6a75d1ed
Ryan Dawkins <dawkinssu@gmail.com>
2014-10-05 23:07:25 -0500

    Added junit to the project

--- src/test/BinaryTest.java
+++ src/test/BinaryTest.java
@@ -3,6 +3,10 @@ package test;
 import com.ryanddawkins.unit_testing_lab.Binary;
 import com.ryanddawkins.unit_testing_lab.InvalidBinaryNumberException;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.Ignore;
+
 /**
  * Created by dawkins on 10/2/14.
  */
@@ -11,31 +15,15 @@ public class BinaryTest
 
     public static void main(String[] args)
     {
+        badInputShouldExplode();
+        System.out.println("Bad Input test cases ran successfully.");
 
-        boolean passed = true;
-
-        if(badInputShouldExplode()) {
-            System.out.println("Bad Input test cases ran successfully.");
-        } else {
-            System.out.println("Bad Input test cases failed.");
-            passed = false;
-        }
-
-        if(testingBase10()) {
-            System.out.println("Base 10 test cases ran successfully.");
-        } else {
-            System.out.println("Base 10 test cases failed.");
-            passed = false;
-        }
-
-        if(passed) {
-            System.out.println("All tests passed successfully.");
-        } else {
-            System.out.println("One or more tests failed.");
-        }
+        testingBase10();
+        System.out.println("Base 10 test cases ran successfully.");
     }
 
-    public static boolean badInputShouldExplode()
+    @Test
+    public static void badInputShouldExplode()
     {
         // For this first test, we are testing to see that the Binary class
         // will return an error if we send in invalid input
@@ -45,40 +33,36 @@ public class BinaryTest
         // TODO
         // Write a unit test to check if the Binary class fails for some string of text
         // Example: "Hello World"
-        String helloWorld = "";
         try {
-            Binary binary = new Binary(helloWorld);
-            return false;
-        } catch(InvalidBinaryNumberException e) {
+            String helloWorld = "";
+            Binary t1 = new Binary(helloWorld);
+        } catch (Throwable e) {
+            Assert.assertTrue(e instanceof InvalidBinaryNumberException);
         }
 
         // TODO
         // Write a unit test to check if the Binary class fails for some string of integers
         // Example: "123456789"
-        String numbers = "";
-        try {
-            Binary binary = new Binary(numbers);
-            return false;
-        } catch(InvalidBinaryNumberException e) {
+        try{
+            String numbers = "";
+            Binary t2 = new Binary(numbers);
+        } catch (Throwable e) {
+            Assert.assertTrue(e instanceof InvalidBinaryNumberException);
         }
 
         // TODO
         // Write a unit test to check if the Binary class fails for some empty string
         // Example: ""
-        String emptyString = "";
         try {
-            Binary binary = new Binary(numbers);
-            return false;
-        } catch(InvalidBinaryNumberException e) {
+            String emptyString = "";
+            Binary t3 = new Binary(emptyString);
+        } catch (Throwable e) {
+            Assert.assertTrue(e instanceof InvalidBinaryNumberException);
         }
-
-        // TODO
-        // If the test reached this point we have no more test cases?
-        // What should we return?
-        return true;
     }
 
-    public static boolean testingBase10()
+    @Test
+    public static void testingBase10()
     {
         // Here we are going to test base 10 numbers, and see if the Binary class can represent them correctly.
         // Our integers are not expected to be able to hold negative values. So as a placeholder for t1_expected
@@ -88,19 +72,13 @@ public class BinaryTest
         String t1_input = "0";
         int t1_expected = 0;
         Binary t1_binary = new Binary(t1_input);
-        if(t1_expected != t1_binary.getBase10()) {
-            return false;
-        }
+        Assert.assertEquals(t1_expected, t1_binary.getBase10());
 
         // Case 2: "1" base 2 converts to 1 base 10
         String t2_input = "1";
-        int t2_expected = -1;
-        Binary t2_binary = new Binary(t1_input);
-        if(t2_expected != t2_binary.getBase10()) {
-            return false;
-        }
-
-        return true;
+        int t2_expected = 1;
+        Binary t2_binary = new Binary(t2_input);
+        Assert.assertEquals(t2_expected, t2_binary.getBase10());
     }
 
 }
